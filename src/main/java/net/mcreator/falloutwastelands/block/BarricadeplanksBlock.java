a6f4e95eae0d73cbf071c37848c6d38f17bc213e
Vaedak <anton@Machine-Dieu>
2024-01-03 15:49:33 +0100

    Corrected : -Barricade Transparancy

--- src/main/java/net/mcreator/falloutwastelands/block/BarricadeplanksBlock.java
+++ src/main/java/net/mcreator/falloutwastelands/block/BarricadeplanksBlock.java
@@ -1,6 +1,9 @@
 
 package net.mcreator.falloutwastelands.block;
 
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,12 +20,17 @@ import java.util.Collections;
 
 public class BarricadeplanksBlock extends Block {
 	public BarricadeplanksBlock() {
-		super(BlockBehaviour.Properties.of().ignitedByLava().instrument(NoteBlockInstrument.BASS).sound(SoundType.WOOD).strength(2f, 1.5f));
+		super(BlockBehaviour.Properties.of().ignitedByLava().instrument(NoteBlockInstrument.BASS).sound(SoundType.WOOD).strength(2f, 1.5f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
 	}
 
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
 		return 15;
+	}
+
+	@Override
+	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		return Shapes.empty();
 	}
 
 	@Override
