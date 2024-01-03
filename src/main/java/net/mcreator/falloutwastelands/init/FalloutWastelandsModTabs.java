7ef41e502943f953fda363bdf10249a5ddd9bbda
anton <anton@Machine-Dieu>
2024-01-03 13:13:50 +0100

    MCreator Merge commit '4d351b121aa8318def9685c7a12a3190ba066e19' into 88a8e76a23125cabbd41c9db120492d6a31c1f83

--- src/main/java/net/mcreator/falloutwastelands/init/FalloutWastelandsModTabs.java
+++ src/main/java/net/mcreator/falloutwastelands/init/FalloutWastelandsModTabs.java
@@ -103,8 +103,6 @@ public class FalloutWastelandsModTabs {
 				tabData.accept(FalloutWastelandsModItems.COMBATARMOR_LEGGINGS.get());
 				tabData.accept(FalloutWastelandsModItems.COMBATARMOR_BOOTS.get());
 				tabData.accept(FalloutWastelandsModItems.REVOLVER_32.get());
-				tabData.accept(FalloutWastelandsModItems.TEST_WEAPON.get());
-				tabData.accept(FalloutWastelandsModItems.BASE_GUN_ITEM.get());
 			})
 
 					.build());
@@ -117,6 +115,8 @@ public class FalloutWastelandsModTabs {
 				tabData.accept(FalloutWastelandsModItems.JERRICAN.get());
 				tabData.accept(FalloutWastelandsModItems.CAPS.get());
 				tabData.accept(FalloutWastelandsModItems.COPPERWIRES.get());
+				tabData.accept(FalloutWastelandsModItems.TEST_WEAPON.get());
+				tabData.accept(FalloutWastelandsModItems.BASE_GUN_ITEM.get());
 				tabData.accept(FalloutWastelandsModItems.WASTELANDBRICKITEM.get());
 			})
 
