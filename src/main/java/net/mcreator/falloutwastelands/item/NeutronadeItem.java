273e7ecb1d4973c459e7bfbb13b7bc46c88ba4c8
Vaedak <anton@Machine-Dieu>
2024-01-04 13:54:44 +0100

    Added : -Sheet metal barricade to walls tag

--- src/main/java/net/mcreator/falloutwastelands/item/NeutronadeItem.java
+++ src/main/java/net/mcreator/falloutwastelands/item/NeutronadeItem.java
@@ -1,8 +1,20 @@
 
 package net.mcreator.falloutwastelands.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import javax.annotation.Nullable;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.falloutwastelands.init.FalloutWastelandsModItems;
+
+import java.util.List;
 
 public class NeutronadeItem extends Item {
 	public NeutronadeItem() {
@@ -22,7 +34,7 @@ public class NeutronadeItem extends Item {
 
 	@Override
 	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
-		ItemStack retval = new ItemStack(FalloutWastelandsModItems.DELETED_MOD_ELEMENT.get());
+		ItemStack retval = new ItemStack(FalloutWastelandsModItems.EMPTY_NEUTRONADE_BOTTLE.get());
 		super.finishUsingItem(itemstack, world, entity);
 		if (itemstack.isEmpty()) {
 			return retval;
